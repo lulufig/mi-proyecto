38ad3b9228070ffc43172436baba36301064b9bd
Sofia Medina <sm911682@gmail.com>
2025-10-08 17:59:02 -0300

    Agrego seccion Hero con imagen y titulo personalizado

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
-
+import '../components/styles/Main.css'
 
 const Main = () => {
   return (
     <main className='main'>
+      <section className='hero'>
+        <h1 className='hero-title'>COFFEE KALDI</h1>
+      </section>
       
 
     </main>
