aea8303f8db4bad6bda00f98ae8e3399e761442e
Sofia Medina <sm911682@gmail.com>
2025-10-20 21:31:30 -0300

    Actualizacion en el hero

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,18 +4,23 @@ import '../components/styles/Main.css'
 const Main = () => {
   return (
     <main className='main'>
-      <section className='hero'>
+      
+    <section className='hero'>
+    
 
-        {/*Contenido superpuesto */}
+       {/*Contenido superpuesto */}
         <div className='hero-content'>
-          <h2 className='hero-subtitle'>Descubre el sabor autentico del cafe artesanal en un ambiente unico.</h2>
+          <div className='hero-title-group'>
+            <h1 className='hero-title'>COFFEE KALDI</h1>
+          </div>
+          <p className='hero-subtitle'>Descubre el sabor autentico del cafe artesanal en un ambiente unico.</p>
 
           <div className='hero-buttons'>
             <a href='#reservas' className='btn btn-primary'>Reservar ahora</a>
             <a href='#menu' className='btn btn-secondary'>Ver menú</a>
           </div>
         </div>
-        
+       
       </section>
       
 
